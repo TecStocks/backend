189ba16236c8c02f7243598bb080262d97331cff
Rafael Pereira <rafahsanttos96@gmail.com>
2022-05-15 16:12:44 -0300

    checking deleted users

--- src/services/CheckDeletedUsers.js
+++ src/services/CheckDeletedUsers.js
@@ -3,16 +3,23 @@ const RemovedUser = require('../models/RemovedUserSchema')
 const populate = require('../services/populateDB')
 
 
-  
-const CheckDeletedUsersService = async () => {
-    populate()
-    try {
-      const users = await User.find()
-      const removed = await RemovedUser.find()
-      console.log(users)
 
+const CheckDeletedUsersService = async () => {
+    await populate()
+    try {
       
+      const removed = await RemovedUser.find()
+
+     for await (removeds of removed){
+         user = await User.findOne({_id: removeds.cod})
+         
+         if (user) {
+            await User.deleteOne({ _id: removeds.cod })
+         }
+     }
+     
     } catch (err) {
+
     }
   }
 

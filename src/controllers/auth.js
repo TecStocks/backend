f81d078d080d8b5891370b03ff800c572146ac48
Rafael Bettini <rafael8885326@gmail.com>
2022-06-07 02:19:15 -0300

    verificando teste unitario

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,10 @@
 const User = require('../models/UserSchema')
+const checkLogin = require('./validateLogin')
 
 const authenticate = async (login, password) => {
-  let data = await User.findOne({ Login: login, Password: password })
+  if(checkLogin(login) && checkLogin(password)){
+    let data = await User.findOne({ Login: login, Password: password })
+  }
   return data
 }
 
--- src/controllers/validateLogin.js
+++ src/controllers/validateLogin.js
@@ -1,3 +1,4 @@
+//Verifica se o valor do login não é nulo
 const checkNotNull=  (login) =>{
     return login != null && login != undefined
   }
